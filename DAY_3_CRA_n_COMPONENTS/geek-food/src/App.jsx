a34fefd2c430bbdbaab59f8d106a6cb0588dd8bc
SJ22032003 <jaryalshaurya2203@gmail.com>
2024-04-24 14:15:55 +0530

    Add React components and configuration files

---
+++ DAY_3_CRA_n_COMPONENTS/geek-food/src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from "./components/footer";
+import Header from "./components/header";
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <main
+        id="main-content"
+      >
+        Our Main Content
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ DAY_3_CRA_n_COMPONENTS/geek-food/src/components/footer/index.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div
+      id="main-footer"
+    >Footer</div>
+  )
+}
+
+export default Footer
