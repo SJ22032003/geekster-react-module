a62ed2c426d11f0175bb5b8af25e194ed094e6a4
SJ22032003 <jaryalshaurya2203@gmail.com>
2024-04-24 23:05:37 +0530

    Refactor and add new files and code for the Geek Food project

---
+++ geek-food/src/components/Header/index.jsx
@@ -0,0 +1,36 @@
+import "./styles.css";
+import styles from "./css_style.module.css";
+import reactImg from "../../assets/react.svg";
+
+function Header() {
+  const navItem = ["Home", "Quote", "Restaurant", "Food"];
+
+  const inlineStyle = {
+    backgroundColor: "#111827",
+    color: "#fff",
+    padding: "20px 40px",
+  };
+
+  return (
+    <div style={inlineStyle}>
+      <nav className="navbarItem">
+        <section className={styles.sectionContainer}>
+          <img src={reactImg} alt="logo" />
+          <h2>GeekFood</h2>
+        </section>
+        <section>
+          <ul>
+            {navItem.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </section>
+        <section className={styles.sectionContainer}>
+          <button>Get Started</button>
+        </section>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
