9b1478d1a3607a36ebce59c1d810efc096aa0e36
SJ22032003 <jaryalshaurya2203@gmail.com>
2024-04-25 21:57:30 +0530

    Refactor Profile component to add icons and improve code readability

--- DAY_4_PROPS/src/components/Profile/index.jsx
+++ DAY_4_PROPS/src/components/Profile/index.jsx
@@ -1,15 +1,19 @@
 import Card from "../Quote/Card";
+import { Bs6SquareFill } from "react-icons/bs";
+import { CiAirportSign1 } from "react-icons/ci";
 
 function Profile({ name = "SJ", age, isAdult }) {
   return (
     <div id="profile-container">
+      <Bs6SquareFill /> 
+      <CiAirportSign1 />
       <h2>Profile</h2>
       <p>{name}</p>
       <p>{age}</p>
-      <p>Is user adult  = { isAdult ? "yes": "no" }</p>
+      <p>Is user adult = {isAdult ? "yes" : "no"}</p>
       <Card quote={"hi from profile"} author="geekster" />
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
