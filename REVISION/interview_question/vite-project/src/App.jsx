da7a6e83fb9e3989dec02f368abf77eef8a494d1
shobhit_jaryal <shobhitjaryal2203@yahoo.com>
2024-05-04 22:04:19 +0530

    Refactor and add files for the Vite project

---
+++ REVISION/interview_question/vite-project/src/App.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [dataList, setDataList] = useState([]);
+  const [userInput, setUserInput] = useState("");
+  const [userList, setUserList] = useState([]);
+
+  // fetch data from api;
+  const getUserData = async () => {
+    await fetch("https://jsonplaceholder.typicode.com/users")
+      .then(async (res) => {
+        const userDataList = await res.json();
+        setDataList(userDataList);
+        setUserList(userDataList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // filter the userList
+  const filteringUserList = () => {
+    const filteredList = dataList.filter(user => user.name.startsWith(userInput));
+    setUserList(filteredList);
+  }
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  useEffect(() => {
+    filteringUserList()
+  }, [userInput])
+
+  return (
+    <>
+      <main>
+        {/* input for searching names */}
+        <section>
+          <input type="text" placeholder="search for users" value={userInput}
+            onChange={(e) => setUserInput(e.target.value) }
+          />
+        </section>
+        {/* mapping my users */}
+        <section>
+          {userList.map((user) => {
+            return <p key={user.id}>{user.name}</p>;
+          })}
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
