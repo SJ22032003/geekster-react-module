357d8655be281284a1a5bf715b1ac3223d722138
shobhit_jaryal <shobhitjaryal2203@yahoo.com>
2024-05-13 22:50:24 +0530

    Update markdown rendering in App component

--- PROJECT/react-markdown/src/App.jsx
+++ PROJECT/react-markdown/src/App.jsx
@@ -1,27 +1,34 @@
 import "./App.css";
 import Markdown from "react-markdown";
+import  { useState} from "react";
 
 function App() {
-  const markdown = `
+
+  const [markdown, setMarkdown] = useState(``);
+
+
+
+//   const markdown = `
   
-  # this is heading
+//   # this is heading
   
-  A paragraph with *emphasis* and **strong importance**.
+//   A paragraph with *emphasis* and **strong importance**.
 
-> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+// > A block quote with ~strikethrough~ and a URL: https://reactjs.org.
 
-* Lists
-* [ ] todo
-* [x] done
+// * Lists
+// * [ ] todo
+// * [x] done
 
-A table:
+// A table:
 
-| a | b |
-| - | - |
-`;
+// | a | b |
+// | - | - |
+// `;
 
   return (
     <>
+      <textarea name="mark-dwon" id="" cols="30" rows="5" onChange={(e) => setMarkdown(e.target.value)} value={markdown}></textarea>
       <Markdown>{markdown}</Markdown>
     </>
   );
