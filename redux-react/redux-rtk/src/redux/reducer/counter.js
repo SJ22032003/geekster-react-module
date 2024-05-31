4004fc8a462c41ca11aa7b522a547c5e0b50b90b
SJ22032003 <jaryalshaurya2203@gmail.com>
2024-05-31 22:53:06 +0530

    feat: Update incrementByAmount action to accept an object with count and demo properties

--- redux-react/redux-rtk/src/redux/reducer/counter.js
+++ redux-react/redux-rtk/src/redux/reducer/counter.js
@@ -16,7 +16,8 @@ export const counterSlice = createSlice({
       state.value -= 1
     },
     incrementByAmount: (state, action) => {
-      state.value += action.payload
+      state.value += action.payload.count,
+      state.demo = action.payload.demo
     },
   },
 })
