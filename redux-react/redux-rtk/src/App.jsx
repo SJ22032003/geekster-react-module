de8214666c1b8f1989e1c236286292493ab98054
shobhit_jaryal <shobhitjaryal2203@yahoo.com>
2024-06-01 21:20:11 +0530

    feat: Add user slice and async thunk for fetching users from API

--- redux-react/redux-rtk/src/App.jsx
+++ redux-react/redux-rtk/src/App.jsx
@@ -1,11 +1,19 @@
 import { useSelector, useDispatch } from "react-redux";
 import { increment, incrementByAmount } from "./redux/reducer/counter";
+import { getAllName, getUsersFromApi } from "./redux/reducer/user";
 import "./App.css";
 
 function App() {
   const { demo, value } = useSelector((state) => state.counter);
-
   const dispatch = useDispatch();
+
+  const handleUsers = () => {
+    dispatch(getUsersFromApi());
+  };
+
+  const { users, loading, error } = useSelector((state) => state.user);
+
+  console.log(users, loading, error);
 
   return (
     <>
@@ -13,9 +21,21 @@ function App() {
       <h2>{demo ? "yes" : "no"}</h2>
       <button onClick={() => dispatch(increment())}>Increase</button>
 
-      <button onClick={() => dispatch(incrementByAmount({ count: 10, demo: false }))}>
+      <button
+        onClick={() => dispatch(incrementByAmount({ count: 10, demo: false }))}
+      >
         increase by 10
       </button>
+
+      <section>
+        <button onClick={handleUsers}>Get users from api</button>
+
+        <button
+          onClick={() => dispatch(getAllName())}
+        >Get only names</button>
+
+        <div>{loading ? <h1>Loading</h1> : <>{JSON.stringify(users)}</>}</div>
+      </section>
     </>
   );
 }
--- redux-react/redux-rtk/src/redux/store.js
+++ redux-react/redux-rtk/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "./reducer/counter";
+import { userReducer } from "./reducer/user";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    user: userReducer,
   },
 });
