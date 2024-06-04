02903a5353514e7eb761684593b58b20d2fdf0dd
SJ22032003 <jaryalshaurya2203@gmail.com>
2024-06-04 21:46:45 +0530

    feat: Refactor deleteComment function to remove unnecessary parameter

--- MCT/question/src/components/NestedComments.jsx
+++ MCT/question/src/components/NestedComments.jsx
@@ -15,7 +15,7 @@ function NestedComments({ data }) {
           <div key={comment.id} style={{ marginLeft: "100px", border: "1px solid red" }}>
             <h1>{comment.comment}</h1>
             <button
-              onClick={() => deleteComment(comment.id, comments)}
+              onClick={() => deleteComment(comment.id)}
             >Delete</button>
             {comment.replies.length > 0 ? (
               <NestedComments data={comment.replies} />
