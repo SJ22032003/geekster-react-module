61c12e78bbaab6a928dbd4c53cfe402c139f7768
SJ22032003 <jaryalshaurya2203@gmail.com>
2024-06-04 21:43:08 +0530

    feat: Add NestedComments component for displaying nested comments

--- MCT/question/src/App.jsx
+++ MCT/question/src/App.jsx
@@ -1,13 +1,45 @@
-import './App.css'
+// import './App.css'
 // import Accordion from './components/Accordion'
-import Tab from './components/Tab'
+// import Tab from './components/Tab'
+import NestedComments from './components/NestedComments'
+
+const data = [
+  {
+    id: 1,
+    comment: "comment 1",
+    replies: [
+      {
+        id: 11,
+        comment: "comment 1 reply 1",
+        replies: []
+      },
+      {
+        id: 12,
+        comment: "comment 1 reply 2",
+        replies: [
+          {
+            id: 121,
+            comment: "comment 1 reply 2 reply 1",
+            replies: []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 2,
+    comment: "comment 2",
+    replies: []
+  }
+]
 
 function App() {
   
   return (
     <>
       {/* <Accordion /> */}
-      <Tab />
+      {/* <Tab /> */}
+      <NestedComments data={data} />
     </>
   )
 }
---
+++ MCT/question/src/components/NestedComments.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function NestedComments({ data }) {
+  const [comments, setComments] = useState(data);
+
+  const deleteComment = (id) => {
+   const filteredComment = comments.filter(ele => ele.id !== id);
+   setComments(filteredComment);
+  }
+
+  return (
+    <div>
+      {comments.map((comment) => {
+        return (
+          <div key={comment.id} style={{ marginLeft: "100px", border: "1px solid red" }}>
+            <h1>{comment.comment}</h1>
+            <button
+              onClick={() => deleteComment(comment.id, comments)}
+            >Delete</button>
+            {comment.replies.length > 0 ? (
+              <NestedComments data={comment.replies} />
+            ) : (
+              <></>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default NestedComments;
